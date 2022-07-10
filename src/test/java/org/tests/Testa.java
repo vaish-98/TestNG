1c1b2bccd2a72d94959ce06653e6c60c56f34eae
vaish-98 <vaihinav.98@gmail.com>
2022-07-10 09:59:19 +0530

    push to master

---
+++ src/test/java/org/tests/Testa.java
@@ -0,0 +1,14 @@
+package org.tests;
+
+public class Testa {
+	private void tc1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void tc2() {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+}
